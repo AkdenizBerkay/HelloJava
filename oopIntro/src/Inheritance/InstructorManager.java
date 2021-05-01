d48a874b04ea75efe23c6e061be0596ae385faf1
Berkay Akdeniz <akdenizberkay@gmail.com>
2021-05-01 21:42:24 +0300

    test

--- oopIntro/src/Inheritance/InstructorManager.java
+++
@@ -1,8 +0,0 @@
-package Inheritance;
-
-public class InstructorManager extends UserManager{
-	public void getBranch(Instructor instructor) {
-	System.out.println(instructor.getFirstName()+ " " + instructor.getLastName() + " Branþý: " + instructor.getBranch());
-	}
-}
- 
